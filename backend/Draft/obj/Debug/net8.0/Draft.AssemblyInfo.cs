166795c2c9980a6dbffaff7e433b12228ce582c9
MacKosh <illay.chuilov12@gmail.com>
2025-05-25 12:28:12 +0300

    docker_2

--- backend/Draft/obj/Debug/net8.0/Draft.AssemblyInfo.cs
+++ backend/Draft/obj/Debug/net8.0/Draft.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Draft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d187b5a34b650c0df502b32833f6e5505494f22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Draft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Draft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
