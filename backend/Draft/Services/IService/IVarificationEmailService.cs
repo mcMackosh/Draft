c7793e43f4d6a3fa4e51bf041c6fe44db79e7ff9
MacKosh <illay.chuilov12@gmail.com>
2025-05-25 12:35:40 +0300

    Додано backend як звичайну папку

--- backend/Draft/Services/IService/IVarificationEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Draft.Data.Model;
-
-namespace Draft.Services.IService
-{
-    public interface IVarificationEmailService
-    {
-        Task SendVerificationEmailAsync(string email, string verificationCode);
-    }
-}
